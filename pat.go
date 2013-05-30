d0ef895e6e6a8e5e285c38ddbc3574fdd78e960e
Martin Gammelsæter <martigam@stud.ntnu.no>
2013-05-30 22:08:24 +0200

    Lots of work on core.
    Starting to flesh out how the core should work.
    Probably still lots of stupid choices and weird idioms.

--- pat.go
+++ pat.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	// TODO(mg): I don't like these kinds of imports, seems magical.
+	// TODO(mg): I don't like these kinds of imports, seems magical and prone
+  // to breakage. Host my own fork or something, preferably no remote imports
+  // at all.
 	"github.com/nsf/termbox-go"
 
 	"runtime"
 )
 
-func drawLine(y, cl int) {
-	termw, _ := termbox.Size()
-
-	for i := 0; i < termw; i++ {
-		termbox.SetCell(i, y, 'l', termbox.Attribute(cl), termbox.Attribute(cl))
-	}
-}
+const(
+  VERSION = "0.1.12"
+)
 
 func main() {
 	// We use all available cores.
@@ -21,6 +19,7 @@ func main() {
 
 	err := termbox.Init()
 	if err != nil {
+    // TODO(mg): Gracefully quit, don't panic.
 		panic(err)
 	}
 	defer termbox.Close()
@@ -28,19 +27,32 @@ func main() {
 	// TODO(mg): Find out which one we want to use.
 	termbox.SetInputMode(termbox.InputEsc)
 
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+  cv = STARTUP_VIEW
+  views[cv].renderFunc()
 
-	drawLine(2, 3)
-	termbox.Flush()
-
+  // TODO(mg): Do loop in each view instead of single mainloop?
+  // Specific stuff, then default: Handle menus etc.
 	// Main loop.
 main_loop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
+      // TODO(mg): Can we do something to break up this if/else chain nicely?
+      // Global keys like changing view
 			if ev.Key == termbox.KeyCtrlC {
 				break main_loop
-			}
+			} else if ev.Ch == 'd' {
+        cv = "directoryView"
+        cvRender()
+
+      // All other keys should be handled by the current view.
+      } else {
+        cvKeyHandler(&ev)
+      }
+    case termbox.EventResize:
+      cvRender()
+    case termbox.EventError:
+      panic(ev.Err)
 		}
 	}
 }
